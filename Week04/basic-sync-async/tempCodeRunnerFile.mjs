e313a58a820cc706fc46211bbc48d5df48343a82
Fernanda Passos <fernanda@midiacom.uff.br>
2025-09-29 17:35:27 +0100

    Basic async examples.

---
+++ Week04/basic-sync-async/tempCodeRunnerFile.mjs
@@ -0,0 +1,5 @@
+longOperationAsyncWithPromise(VAL)   // Promise<Number>
+//     .then(processResult);            // Promise<String>
+
+// let p = longOperationAsyncWithPromise(VAL);
+// p.then(processResult);
